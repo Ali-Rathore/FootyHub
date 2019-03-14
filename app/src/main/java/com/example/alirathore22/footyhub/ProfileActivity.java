13c1be24afa8a42be6f72e23bf9f31476828c898
Ali-Rathore <alirthr@gmail.com>
2019-03-14 21:45:54 +0500

    news

--- app/src/main/java/com/example/alirathore22/footyhub/ProfileActivity.java
+++ app/src/main/java/com/example/alirathore22/footyhub/ProfileActivity.java
@@ -8,16 +8,37 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.alirathore22.footyhub.api.ApiClient;
+import com.example.alirathore22.footyhub.api.ApiInterface;
+import com.example.alirathore22.footyhub.models.Article;
+import com.example.alirathore22.footyhub.models.News;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String API_KEY = "e0a61c52954d4b72b6f47a427ea29306";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = MainActivity.class.getSimpleName();
 
 
     @Override
@@ -25,9 +46,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(ProfileActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+        LoadJson();
+
+
+
+
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
-
         //to not have home button always highlighted
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);
@@ -59,10 +90,38 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 return false;
             }
         });
+    }
+
+    public void LoadJson(){
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null){
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, ProfileActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(ProfileActivity.this, "No Result!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
 
 
-
-
+            }
+        });
 
     }
 
