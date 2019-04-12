21b293d7f94612da3a0ef5c67fecdfa9f1b60e68
ammarsaqib123 <ammarsaqib123@gmail.com>
2019-04-12 11:52:04 +0500

    activities converted to fragments

--- app/src/main/java/com/example/alirathore22/footyhub/ExtraActivity.java
+++ app/src/main/java/com/example/alirathore22/footyhub/ExtraActivity.java
@@ -5,92 +5,148 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class ExtraActivity extends AppCompatActivity implements View.OnClickListener {
+public class ExtraActivity extends Fragment implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
     private Button buttonLogout;
     private TextView viewEmail;
 
+    private FragmentManager fragmentManager;
+
+    private FragmentTransaction ft;
+    private Fragment frag;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_extras);
-        TextView title = (TextView) findViewById(R.id.activityTitle4);
+//        setContentView(R.layout.activity_extras);
+
+        fragmentManager = getFragmentManager();
+
+        if (fragmentManager == null)
+        {
+            Toast.makeText(getContext(), "fragment manager null", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            ft = fragmentManager.beginTransaction();
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        if (container != null) {
+            container.removeAllViews();
+        }
+
+        View view = inflater.inflate(R.layout.activity_extras, container, false);
+
+        TextView title = (TextView) view.findViewById(R.id.activityTitle4);
         title.setText("This is the Extras Activity");
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) view.findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         //to not have home button always highlighted
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(4);
         menuItem.setChecked(true);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.ic_home:
-                        Intent intent0 = new Intent(ExtraActivity.this, ProfileActivity.class);
-                        startActivity(intent0);
-                        break;
-                    case R.id.ic_fixtures:
-                        Intent intent1 = new Intent(ExtraActivity.this, FixturesActivity.class);
-                        startActivity(intent1);
-                        break;
-                    case R.id.ic_livestream:
-                        Intent intent2 = new Intent(ExtraActivity.this, LiveStreamActivity.class);
-                        startActivity(intent2);
-                        break;
-                    case R.id.ic_tables:
-                        Intent intent3 = new Intent(ExtraActivity.this, TablesActivity.class);
-                        startActivity(intent3);
-                        break;
-                    case R.id.ic_extras:
+        set_botton_navigation_listener(bottomNavigationView);
 
-                        break;
-                }
-                return false;
-            }
-        });
         firebaseAuth = FirebaseAuth.getInstance();
-        if(firebaseAuth.getCurrentUser() == null){
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
-
-        }
+//        if(firebaseAuth.getCurrentUser() == null){
+//
+////            finish();
+////            startActivity(new Intent(this, LoginActivity.class));
+//
+//        }
 
         //setting user
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        buttonLogout = findViewById(R.id.buttonLogout);
-        viewEmail = findViewById(R.id.viewEmail);
+        buttonLogout = view.findViewById(R.id.buttonLogout);
+        viewEmail = view.findViewById(R.id.viewEmail);
 
         buttonLogout.setOnClickListener(this);
 
         //getting email for current user and setting in Text View.
         viewEmail.setText("Select the option "+user.getEmail());
+
+        return view;
     }
 
     @Override
     public void onClick(View view) {
         if(view == buttonLogout){
             //logging out current user and starting the login Activity
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
+//            firebaseAuth.signOut();
+//            finish();
+//            startActivity(new Intent(this, LoginActivity.class));
         }
     }
 
-
+    public void set_botton_navigation_listener(BottomNavigationView bottomNavigationView)
+    {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        startActivity(new Intent(getContext(), ProfileActivity.class));
+                        break;
+                    case R.id.ic_fixtures:
+////                        Intent intent = new Intent(ProfileActivity.this, FixturesActivity.class);
+////                        startActivity(intent);
+                        frag = new FixturesActivity();
+                        ft.replace(R.id.mainLayout, frag);
+//                        ft.addToBackStack(stack);
+                        ft.commit();
+                        break;
+                    case R.id.ic_livestream:
+//                        Intent intent1 = new Intent(ProfileActivity.this, LiveStreamActivity.class);
+//                        startActivity(intent1);
+                        frag = new LiveStreamActivity();
+                        ft.replace(R.id.mainLayout, frag);
+//                        ft.addToBackStack(stack);
+                        ft.commit();
+                        break;
+                    case R.id.ic_tables:
+//                        Intent intent2 = new Intent(ProfileActivity.this, TablesActivity.class);
+//                        startActivity(intent2);
+                        frag = new TablesActivity();
+                        ft.replace(R.id.mainLayout, frag);
+//                        ft.addToBackStack(stack);
+                        ft.commit();
+                        break;
+                    case R.id.ic_extras:
+//                        frag = new ExtraActivity();
+//                        ft.replace(R.id.mainLayout, frag);
+////                        ft.addToBackStack(stack);
+//                        ft.commit();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
 
 }
